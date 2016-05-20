499e482e61c7ba98eaf89e8ab8d6da28da1ce8eb
Machine3 <machine3@Admins-Mac-mini-2.local>
2016-05-20 16:32:05 +0530

    no message

--- app/src/main/java/com/example/machine2/movietime/MainActivity.java
+++ app/src/main/java/com/example/machine2/movietime/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.GridView;
 
@@ -41,10 +40,7 @@ public class MainActivity extends BaseActivity implements MovieAdapter {
         movieManager = new MovieManager();
         movieManager.movieManager(this);
 
-        //Onclick of Gridview
-        //gridView.setOnItemClickListener(new GridviewClick());
     }
-
 
     @Override
     public void setImageAdapter(MovieImageAdapter imageAdapter) {
