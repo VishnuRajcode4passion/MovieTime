004c0ec8a640ea4c3facf439aaa07ceb8198af51
VishnuRajcode4passion <>
2016-05-20 14:14:07 +0530

    commit

---
+++ app/src/main/java/com/example/machine2/moviesss/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.machine2.moviesss;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+//commit
+}
